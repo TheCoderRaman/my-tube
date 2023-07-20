1e61899cfc52baa1b82a940666cda24324307ce8
Animax Dev <arkanimetube@gmail.com>
2023-07-20 16:33:30 +0530

    Updated import double quotes to single

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
-import { initReactI18next } from "react-i18next";
-import { config, resources } from "./Data/translation";
-import { localStorage as ls} from "./Modules/LocalStorageSlim";
+import { initReactI18next } from 'react-i18next';
+import { config, resources } from './Data/translation';
+import { localStorage as ls} from './Modules/LocalStorageSlim';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 config.selected = ls.get('locale',{ 
