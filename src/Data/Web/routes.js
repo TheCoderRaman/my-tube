ca70aa53c189bee60b63d89a5709f4035ccdc34d
Animax Dev <arkanimetube@gmail.com>
2023-07-20 16:17:31 +0530

    Added application routes collection

---
+++ src/Data/Web/routes.js
@@ -0,0 +1,19 @@
+/**
+ * This module give us a single location for altering any
+ * route in the entire application at any point of 
+ * time in the future.
+ * 
+ * @var const Object routes
+ */
+export const routes = {
+    Error: {
+        name: "Error",
+        path: "*",
+        element: <>Error</>
+    },
+    Home: {
+        name: "Home",
+        path: '/',
+        element: <>Home</>
+    }
+};
