1f53b94477950d6f53b8d94ea588df458a0922cd
Animax Dev <arkanimetube@gmail.com>
2023-07-20 16:31:09 +0530

    Updated import double quotes to single

--- src/Data/translation.js
+++ src/Data/translation.js
@@ -1,6 +1,6 @@
-import i18n from "i18next";
-import { en } from "./Locales/en";
-import { hi } from "./Locales/hi";
+import i18n from 'i18next';
+import { en } from './Locales/en';
+import { hi } from './Locales/hi';
 
 /**
  * This module contain all translations
