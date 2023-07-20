22e34429920532bac163a3ceec820fa413a515c1
Animax Dev <arkanimetube@gmail.com>
2023-07-20 16:32:09 +0530

    Added new router web for handling routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
 import React from 'react';
+import Web from './Router/Web';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">MyTube</div>
+        <BrowserRouter>
+          <Web />
+        </BrowserRouter>
     )
 }
 
