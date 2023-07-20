e3d3f205bdd68e6a699d3d984a520475e1af66cc
Animax Dev <arkanimetube@gmail.com>
2023-07-20 16:15:05 +0530

    Added setting file

---
+++ src/Data/settings.js
@@ -0,0 +1,12 @@
+/**
+ * All application settings will be defined here.
+ * 
+ * @var const settings
+ */
+export const settings = {
+    app: {
+        name: 'My Tube',
+        shorName: 'MyTube',
+        summary: 'Modern MyTube Application in React JS with Material UI'
+    }
+};
