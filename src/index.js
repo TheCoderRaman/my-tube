1ddda0a45fbe0630b4641330b51f53435ac5887c
Animax Dev <arkanimetube@gmail.com>
2023-07-20 16:33:10 +0530

    Added redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import App from './App';
 import React from 'react';
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './Redux/stores/store';
+import { createRoot } from 'react-dom/client';
 
 const root = createRoot(
   document.getElementById("root")
@@ -9,8 +10,8 @@ const root = createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
        <App />
-    </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
